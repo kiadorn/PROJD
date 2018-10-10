1c134e3b64e70977e30791b10ed08ff5070eea1c
Alexander Hall <alexander.hall.lanerfeldt@gmail.com>
2018-10-10 15:55:20 +0200

    host test

---
+++ Assets/CustomNetworkDiscovery.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkDiscovery : NetworkDiscovery {
+
+    public override void OnReceivedBroadcast(string fromAddress, string data)
+    {
+        Debug.Log("Found shit");
+        NetworkManager.singleton.networkAddress = fromAddress;
+        NetworkManager.singleton.StartClient();
+    }
+
+    
+}
---
+++ Assets/CustomNetworkLobbyManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CustomNetworkLobbyManager : NetworkLobbyManager {
+
+    public GameObject menuView;
+    public GameObject hostView;
+    public GameObject findView;
+       
+
+	public void StartNewServer()
+    {
+        menuView.SetActive(false);
+        hostView.SetActive(true);
+        StartServer();
+    }
+
+    public void JoinServer()
+    {
+        menuView.SetActive(false);
+        findView.SetActive(true);
+        StartClient();
+    }
+}
