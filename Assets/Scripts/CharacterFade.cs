71d06fccb7847a34489ed5086216ffa996368bb5
leas9594 <38495831+leas9594@users.noreply.github.com>
2018-10-11 16:09:35 +0200

    change

---
+++ Assets/Scripts/CharacterFade.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterFade : MonoBehaviour {
+    public float fadeSpeed = 0.2f;
+    // Use this for initialization
+
+    public Material standardMaterial;
+    public Material transparentMaterial;
+
+
+    void Start () {
+        ChangeTransparency();
+        ChangeTransparency();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey( "w"))
+            ChangeTransparency();
+    }
+
+    private void ChangeTransparency()
+    {
+        if (this.gameObject.GetComponent<MeshRenderer>().material == standardMaterial)
+        {
+            this.gameObject.GetComponent<MeshRenderer>().material = transparentMaterial;
+        }
+        else if (this.gameObject.GetComponent<MeshRenderer>().material == transparentMaterial)
+        {
+            this.gameObject.GetComponent<MeshRenderer>().material = standardMaterial;
+        }
+        Debug.Log(this.gameObject.GetComponent<MeshRenderer>().material);
+    }
+}
