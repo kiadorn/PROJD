c31e9da9c2a4ec900b1f94cb2683ab95f792bc38
leas9594 <38495831+leas9594@users.noreply.github.com>
2018-10-12 15:09:33 +0200

    animations

---
+++ Assets/Scripts/AnimationTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour {
+
+    public Animator animator;
+    public float chargeSpeed = 0.01f;
+    public float speed=0;
+
+    // Use this for initialization
+    void Start () {
+        animator.SetFloat("Velocity", 0);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        ChangeStance();
+
+        if (Input.GetKey("space"))
+        {
+            animator.SetFloat("Velocity", 1);
+        }
+
+        else
+        {
+            animator.SetFloat("Velocity", 0);
+        }       
+    
+    }
+
+    private void ChangeStance()
+    {
+        if (Input.GetKey("w"))
+        {
+            if(speed<1)
+                speed = speed + chargeSpeed;
+
+            animator.SetLayerWeight(1, speed);//tänk på hur många layers det finns
+            Debug.Log(speed);
+        }
+        else if (Input.GetKey("s"))
+        {
+            if (speed > 0)
+                speed = speed - chargeSpeed;
+
+            animator.SetLayerWeight(1, speed);
+            Debug.Log(speed);
+        }
+    }
+
+
+}
+
+
