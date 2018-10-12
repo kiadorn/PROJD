0b090f3adc8bf84042a67ab0a632e089d9e4f25e
Couchpirate <chuu@live.se>
2018-10-12 15:20:38 +0200

    Optimiserat och fixat lite grejs

--- Assets/Scripts/MaterialSwap.cs
+++ Assets/Scripts/MaterialSwap.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MaterialSwap : MonoBehaviour {
 
     public float speedMultiplier;
-    float timer;1
+    [Range(-1, 1)] float fade;
 
     MeshRenderer meshr;
     bool fadeOut = false;
@@ -14,28 +14,55 @@ public class MaterialSwap : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         meshr = gameObject.GetComponent<MeshRenderer>();
-        timer = meshr.material.GetFloat("Vector1_6C82E8EC");
     }
 	
 	// Update is called once per frame
 	void Update () {
+
+        Ray ray = new Ray(transform.position, new Vector3(0, -100, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Texture2D textureMap = (Texture2D)hit.transform.GetComponent<Renderer>().material.mainTexture;
+            var pixelUV = hit.textureCoord;
+            pixelUV.x *= textureMap.width;
+            pixelUV.y *= textureMap.height;
+
+            print("x=" + pixelUV.x + ",y=" + pixelUV.y + " " + textureMap.GetPixel((int)pixelUV.x, (int)pixelUV.y));
+        }
+
         if (Input.GetKey(KeyCode.Alpha1)){
-            timer = 1;
-            meshr.material.SetFloat("Vector1_6C82E8EC", timer);
             fadeIn = true;
+            fadeOut = false;
         }
         if (Input.GetKey(KeyCode.Alpha2))
         {
-            timer = -1;
-            meshr.material.SetFloat("Vector1_6C82E8EC", timer);
+            fadeIn = false;
+            fadeOut = false;
         }
         if (Input.GetKey(KeyCode.Alpha3))
         {
-            timer = -1;
-            meshr.material.SetFloat("Vector1_6C82E8EC", timer);
             fadeOut = true;
+            fadeIn = false;
+        }
+        // om fadeIn är sann, minska timer, om fadeOut är sann, öka timern, clampa mellan -1 och 1. 
+        
+        if(fadeIn == true)
+        {
+            fade += Time.deltaTime * speedMultiplier;
+            float output = Mathf.Clamp(fade, -1, 1);
+            meshr.material.SetFloat("Vector1_6C82E8EC", output);
+        }
+        else if(fadeOut == true)
+        {
+            fade -= Time.deltaTime * speedMultiplier;
+            float output = Mathf.Clamp(fade, -1, 1);
+            meshr.material.SetFloat("Vector1_6C82E8EC", output);
         }
 
+
+        /*
         if (fadeIn == true)
         {
             if (timer > -1)
@@ -55,6 +82,7 @@ public class MaterialSwap : MonoBehaviour {
             }
             else fadeOut = false;
         }
+        */
         //("Vector1_6C82E8EC")
     }
 }
