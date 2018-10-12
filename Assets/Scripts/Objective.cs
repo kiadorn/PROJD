fe8e91ac37c7ab4bd0443d3976f5bcacf8c36887
Alexander Hall <alexander.hall.lanerfeldt@gmail.com>
2018-10-12 13:26:22 +0200

    deleted fluff

--- Assets/Scripts/Objective.cs
+++ Assets/Scripts/Objective.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Objective : MonoBehaviour {
+public class Objective : NetworkBehaviour {
 
     private ObjectiveSpawner _spawner = null;
 
