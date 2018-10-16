3229a8c7ccaa86cedfa4c55e872baaa7c890d0ee
Alexander Hall <alexander.hall.lanerfeldt@gmail.com>
2018-10-16 10:41:24 +0200

    Update Assets/Scripts/ServerStatsManager.cs

--- Assets/Scripts/ServerStatsManager.cs
+++ Assets/Scripts/ServerStatsManager.cs
@@ -200,6 +200,7 @@ public class ServerStatsManager : NetworkBehaviour {
         if (isServer)
         {
             RpcSetPlayerMoving(true);
+            RpcSetPlayerShooting(true);
         }
         roundIsActive = true;
     }
