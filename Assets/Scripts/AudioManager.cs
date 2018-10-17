6ee0671c13460947d0439b99ad839371953ca77f
Carl Axel Tollin <iradain@gmail.com>
2018-10-17 11:00:00 +0200

    Sounds

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour {
+
+    //Used to be able to use coroutines
+    static public AudioManager instance;
+    private void Awake() {
+        instance = this;
+    }
+
+    //For 2D-clips where source is irrelevant
+    public static void Play2DClip(EditedClip clip) {
+        PlayGeneral(clip);
+    }
+
+    //For 3D-clips, uses an object to be used as a parent
+    public static void Play3DClip(EditedClip clip, GameObject sourceOfSound) {
+        PlayGeneral(clip, sourceOfSound);
+    }
+
+    //General method for them
+    private static void PlayGeneral(EditedClip clip, GameObject sourceOfSound = null) {
+        GameObject soundClip = new GameObject();
+        soundClip.AddComponent<AudioSource>();
+        if (sourceOfSound != null) {
+            soundClip.transform.SetParent(sourceOfSound.transform);
+            soundClip.transform.localPosition = new Vector3(0, 0, 0);
+            soundClip.GetComponent<AudioSource>().spatialBlend = 1;
+        } 
+        clip.PlayClip(soundClip.GetComponent<AudioSource>());
+        if (!clip.Looping) {
+            instance.StartCoroutine(WaitAndDestroy(soundClip.GetComponent<AudioSource>()));
+        }
+    }
+
+    //Destruction for when clip has finished playing
+    public static IEnumerator WaitAndDestroy(AudioSource source) {
+        while (source.isPlaying || source.time != 0)
+            yield return 0;
+        Destroy(source.gameObject);
+        yield return 0;
+    }
+
+
+
+}
---
+++ Assets/Scripts/EditedClip.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[CreateAssetMenu(fileName = "EditedClip", menuName = "ScriptableObjects/EditedClip")]
+public class EditedClip : ScriptableObject {
+
+    public AudioClip Clip;
+    [Range(-3f, 3f)]
+    public float Pitch = 1;
+    [Range(0f, 1f)]
+    public float Volume = 1;
+    public bool Looping;
+    public AudioMixerGroup AudioMixer;
+
+    private AudioSource _source;
+
+    //Used to play a the Clip
+    public void PlayClip(AudioSource source) {
+        source.pitch = Pitch;
+        source.volume = Volume;
+        source.loop = Looping;
+        source.playOnAwake = false;
+        source.outputAudioMixerGroup = AudioMixer;
+        source.clip = Clip;
+        _source = source;
+        source.Play();
+    }
+
+    public float GetLength() {
+        return Clip.length;
+    }
+
+    public void Pause() {
+        if(_source != null)
+            _source.Pause();
+    }
+
+    public void UnPause() {
+        if (_source != null)
+            _source.UnPause();
+    }
+
+    public void Stop() {
+        if (_source != null)
+            _source.Stop();
+    }
+
+}
