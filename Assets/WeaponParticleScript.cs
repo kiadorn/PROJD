20cf32da435de25a2cf4771615082f347440698b
Couchpirate <chuu@live.se>
2018-10-19 16:16:49 +0200

    I made vfx stuff

---
+++ Assets/WeaponParticleScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponParticleScript : MonoBehaviour {
+
+    float ChargeModifier = 20f;
+    float WeaponCharge;
+    ParticleSystem[] particleChildren;
+    Transform[] children;
+    ParticleSystem shineParticles1, shineParticles2, shineParticles3;
+
+
+	// Use this for initialization
+	void Start () {
+        children = GetComponentsInChildren<Transform>();
+        particleChildren = GetComponentsInChildren<ParticleSystem>();
+        shineParticles1 = particleChildren[1];
+        shineParticles2 = particleChildren[2];
+        shineParticles3 = particleChildren[3];
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            children[1].gameObject.SetActive(!children[1].gameObject.activeSelf);
+
+
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            var spe1 = shineParticles1.emission;
+            var spe2 = shineParticles2.emission;
+            var spe3 = shineParticles3.emission;
+            var spm1 = shineParticles1.main;
+            spe1.rateOverTime = 0;
+            spe2.rateOverTime = 0;
+            spe3.rateOverTime = 0;
+            spm1.startSpeed = 1;
+            shineParticles1.Emit(1000);
+        }
+		
+	}
+}
