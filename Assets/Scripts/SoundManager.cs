363d3ef56482f7fa0d40c9bd018afc843b3830db
wazzen <steven_keay@hotmail.com>
2018-10-19 14:05:02 +0200

    gameplay + audio

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
-    private GameObject goForAudio;
+    private GameObject goForPlayerAudio;
+    private GameObject goForGateAudio;
+    private GameObject goForOrbAudio;
 
     [Header("Global")]
     public EditedClip backgroundMusic;
@@ -13,6 +15,7 @@ public class SoundManager : MonoBehaviour {
     [Header("Movement")]
     public EditedClip runningSound;
     public EditedClip jumpSound;
+    public EditedClip jumpLanding;
     public EditedClip dashSound;
     [Header("Combat")]
     public EditedClip deathSound;
@@ -29,7 +32,9 @@ public class SoundManager : MonoBehaviour {
 
     private void Awake()
     {
-        goForAudio = GameObject.Find("goForAudio");
+        goForPlayerAudio = GameObject.Find("goForPlayerAudio");
+        goForGateAudio = GameObject.Find("goForGateAudio");
+        goForGateAudio = GameObject.Find("goForOrbAudio");
     }
     private void Start()
     {
@@ -41,16 +46,6 @@ public class SoundManager : MonoBehaviour {
         AudioManager.Play2DClip(countdownSound);
     }
 
-    public void PlayAllyPoint()
-    {
-        AudioManager.Play3DClip(allyPoint, goForAudio);
-    }
-
-    public void PlayEnemyPoint()
-    {
-        AudioManager.Play3DClip(enemyPoint, goForAudio);
-    }
-
     public void PlayAllyWin()
     {
         AudioManager.Play2DClip(allyMatchWinner);
@@ -60,4 +55,70 @@ public class SoundManager : MonoBehaviour {
     {
         AudioManager.Play2DClip(enemyMatchWinner);
     }
+
+    public void PlayRunSound()
+    {
+        AudioManager.Play3DClip(runningSound, goForPlayerAudio);
+    }
+
+    public void PlayJumpSound()
+    {
+        AudioManager.Play2DClip(jumpSound);
+    }
+
+    public void PlayLandingSound()
+    {
+        AudioManager.Play3DClip(jumpLanding, goForPlayerAudio);
+    }
+
+    public void PlayDashSound()
+    {
+        AudioManager.Play3DClip(dashSound, goForPlayerAudio);
+    }
+
+    public void PlayDeathSound()
+    {
+        AudioManager.Play3DClip(deathSound, goForPlayerAudio);
+    }
+
+    public void PlayRespawnSound()
+    {
+        AudioManager.Play3DClip(respawnSound, goForPlayerAudio);
+    }
+
+    public void PlayLaserCharge()
+    {
+        AudioManager.Play3DClip(chargeLaser, goForPlayerAudio);
+    }
+
+    public void PlayFireLaser()
+    {
+        AudioManager.Play3DClip(fireLaser, goForPlayerAudio);
+    }
+
+    public void PlayLaserHit()
+    {
+        AudioManager.Play3DClip(hitLaser, goForPlayerAudio);
+    }
+
+    public void PlayGateSound()
+    {
+        AudioManager.Play3DClip(gateAudio, goForPlayerAudio);
+    }
+
+    public void PlayOrbSound()
+    {
+        AudioManager.Play3DClip(orbAudio, goForOrbAudio);
+    }
+
+    public void PlayAllyPoint()
+    {
+        AudioManager.Play3DClip(allyPoint, goForPlayerAudio);
+    }
+
+    public void PlayEnemyPoint()
+    {
+        AudioManager.Play3DClip(enemyPoint, goForPlayerAudio);
+    }
+
 }
