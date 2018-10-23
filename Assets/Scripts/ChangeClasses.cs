4f78c889b9411b1b42b092346fe961ae3a672f4a
leas9594 <38495831+leas9594@users.noreply.github.com>
2018-10-23 13:03:52 +0200

    Masks

---
+++ Assets/Scripts/ChangeClasses.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeClasses : MonoBehaviour {
+
+    public GameObject SoldierMask;
+    public GameObject SorcererMask;
+    public GameObject ScoutrMask;
+    
+
+    public float speedMultyplier = 1f;
+
+    public PlayerController controller;
+
+    private float forwardStartspeed;
+    private float backwardStartspeed;
+    private float strafeStartspeed;  
+
+    // Use this for initialization
+    void Start () {
+        controller = gameObject.GetComponent<PlayerController>();
+        forwardStartspeed = controller.movementSettings.forwardSpeed;
+        backwardStartspeed = controller.movementSettings.backwardSpeed;
+        strafeStartspeed = controller.movementSettings.strafeSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("t"))
+        {
+            speedMultyplier = 1f;
+            SoldierMask.SetActive(true);
+            SorcererMask.SetActive(false);
+            ScoutrMask.SetActive(false);
+        }
+        else if (Input.GetKeyDown("y"))
+        {
+            speedMultyplier = 0.9f;
+            SoldierMask.SetActive(false);
+            SorcererMask.SetActive(true);
+            ScoutrMask.SetActive(false);
+        }
+        else if (Input.GetKeyDown("u"))
+        {
+            speedMultyplier = 1f;
+            SoldierMask.SetActive(false);
+            SorcererMask.SetActive(false);
+            ScoutrMask.SetActive(true);
+        }
+
+        ClassStats();
+
+    }
+
+    public void ClassStats()
+    {
+        controller.movementSettings.forwardSpeed = forwardStartspeed * speedMultyplier;
+        controller.movementSettings.backwardSpeed = backwardStartspeed * speedMultyplier;
+        controller.movementSettings.strafeSpeed = strafeStartspeed * speedMultyplier;
+        
+    }
+
+}
---
+++ Assets/Scripts/CreateLaser.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateLaser : MonoBehaviour { //obselet kod
+
+    public PlayerController controller;
+    public GameObject laser;
+
+    private float forwardStartspeed;
+
+    public float _beamDistance = 0f;
+
+    // Use this for initialization
+    void Start () {
+        controller = gameObject.GetComponent<PlayerController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("z"))
+        {
+            laser.SetActive(true);
+        }
+        if (Input.GetKeyDown("c"))
+        {
+            laser.SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if(_beamDistance<5.0)
+                _beamDistance += Time.deltaTime*0.5f;
+
+            laser.transform.localScale += new Vector3(0, _beamDistance, 0);
+            laser.transform.localPosition += new Vector3(0, 0, _beamDistance);
+        }
+        else
+        {
+            _beamDistance = 0f;
+        }
+        if(Input.GetKeyUp("p")) 
+        {
+            //_beamDistance = 1f;
+            laser.transform.localScale += new Vector3(0, _beamDistance, 0);
+            laser.transform.localPosition += new Vector3(0, 0, _beamDistance);
+        }
+
+    }
+}
