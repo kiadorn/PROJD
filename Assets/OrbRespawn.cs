d914482e9141580247e9650a941a6b0c5c647362
Couchpirate <chuu@live.se>
2018-11-13 17:02:39 +0100

    respawn :white_check_mark:  + decoy death progress

---
+++ Assets/OrbRespawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbRespawn : MonoBehaviour {
+
+    public GameObject Orb;
+    public AnimationCurve orbSize;
+
+    void Start() {
+        Orb.SetActive(true);
+
+    }
+
+    private void Update() {
+        Orb.transform.localScale = new Vector3(orbSize.Evaluate(Time.time/15), orbSize.Evaluate(Time.time/15), orbSize.Evaluate(Time.time/15));
+    }
+
+}
