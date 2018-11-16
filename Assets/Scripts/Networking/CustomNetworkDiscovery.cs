cff9bff28b172952cdfd227662b8779c36bf0179
Carl Axel Tollin <iradain@gmail.com>
2018-11-16 11:01:27 +0100

    NewLobbyTestStart

--- Assets/Scripts/Networking/CustomNetworkDiscovery.cs
+++ Assets/Scripts/Networking/CustomNetworkDiscovery.cs
@@ -1,15 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class CustomNetworkDiscovery : NetworkDiscovery {
 
+    private float timeout = 5f;
+
+    private Dictionary<LanConnectionInfo, float> lanAdresses = new Dictionary<LanConnectionInfo, float>();
+
+    private void Awake() {
+        base.Initialize();
+        base.StartAsClient();
+        StartCoroutine(CleanupExpiredEntries());
+    }
+
+    public void StartBroadcast() {
+        StopBroadcast();
+        base.Initialize();
+        base.StartAsServer();
+    }
+
+    private IEnumerator CleanupExpiredEntries() {
+        while (true) {
+            bool changed = false;
+
+            var keys = lanAdresses.Keys.ToList();
+            foreach(var key in keys) {
+                if(lanAdresses[key] <= Time.time) {
+                    lanAdresses.Remove(key);
+                    changed = true;
+                }
+            }
+            if (changed) {
+                UpdateMatchInfos();
+            }
+
+            yield return new WaitForSeconds(timeout);
+        }
+    }
+
     public override void OnReceivedBroadcast(string fromAddress, string data)
     {
-        Debug.Log("Found shit");
-        NetworkManager.singleton.networkAddress = fromAddress;
-        NetworkManager.singleton.StartClient();
+        //Debug.Log("Found shit");
+        //NetworkManager.singleton.networkAddress = fromAddress;
+        //NetworkManager.singleton.StartClient();
+
+        base.OnReceivedBroadcast(fromAddress, data);
+
+        LanConnectionInfo info = new LanConnectionInfo(fromAddress, data);
+
+        if(lanAdresses.ContainsKey(info) == false) {
+            lanAdresses.Add(info, Time.time + timeout);
+            UpdateMatchInfos(); //Update UI
+            
+        }
+        else {
+            lanAdresses[info] = Time.time + timeout;
+        }
+    }
+
+    private void UpdateMatchInfos() {
+        //GameListController.AddLanMatches(lanAdresses.Keys.ToList());
     }
 
     
---
+++ Assets/Scripts/Networking/LanConnectionInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct LanConnectionInfo{
+
+    public string ipAdress;
+    public int port;
+    public string name;
+
+	public LanConnectionInfo(string fromAdress, string data) {
+        ipAdress = fromAdress.Substring(fromAdress.LastIndexOf(":") + 1, fromAdress.Length - (fromAdress.LastIndexOf(":") + 1));
+        string portText = data.Substring(data.LastIndexOf(":") + 1, data.Length - (data.LastIndexOf(":") + 1));
+        port = 7777;
+        int.TryParse(portText, out port);
+        name = "local";
+    }
+}
