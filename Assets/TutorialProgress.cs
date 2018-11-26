e1c501971c8892597a8966a1aa679b2ce9827f19
Couchpirate <chuu@live.se>
2018-11-26 17:01:19 +0100

    tutorial stuff

---
+++ Assets/TutorialProgress.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialProgress : MonoBehaviour {
+
+    int progress;
+    public GameObject MovementRoom, StealthRoom, ShootRoom, DashRoom, DecoyRoom, ObjectivesRoom, MapRoom;
+
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Alpha1)) {
+            progress++;
+        }
+        print(MovementRoom.transform.Find("Front Wall").transform.Find("Front Door").localPosition);
+        switch(progress) {
+            case 1: MovementRoom.transform.Find("Front Door").transform.Find("Front Door").localPosition = new Vector3(0, -10, 0);
+            break;
+        }
+		
+	}
+}
