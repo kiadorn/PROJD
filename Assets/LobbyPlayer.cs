90eb673a8b387eb65bd103695cebac9bf24da90e
Alexander Hall <alexander.hall.lanerfeldt@gmail.com>
2018-11-29 12:01:45 +0100

    Update LobbyPlayer.cs

--- Assets/LobbyPlayer.cs
+++ Assets/LobbyPlayer.cs
@@ -87,7 +87,7 @@ public class LobbyPlayer : NetworkLobbyPlayer {
     {
         //if (isLocalPlayer && CustomNetworkLobbyManager.singleton.IsClientConnected())
         //CustomNetworkLobbyManager.singleton.client.Disconnect();
-        Debug.LogFormat("LobbyPlayer {0} : OnClientExitLobby", GetComponent<NetworkIdentity>() ? GetComponent<NetworkIdentity>().netId.ToString() : "NO_ID");
+        print("LobbyPlayer : OnClientExitLobby");
 
         lobbyExit.Raise();
         LobbyList._instance.RemovePlayer(this);
