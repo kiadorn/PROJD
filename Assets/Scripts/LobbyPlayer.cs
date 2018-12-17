9f8deb72518b832d55bdf2758cdfcbee4cae900e
Alexander Hall <alexander.hall.lanerfeldt@gmail.com>
2018-12-17 13:59:59 +0100

    Update LobbyPlayer.cs

--- Assets/Scripts/LobbyPlayer.cs
+++ Assets/Scripts/LobbyPlayer.cs
@@ -83,8 +83,8 @@ public class LobbyPlayer : NetworkLobbyPlayer {
     public void OnDestroy()
     {
         print("LobbyPlayer : OnDestroy");
-        if (lobbyPlayerUI)
-            Destroy(lobbyPlayerUI.gameObject);
+        //if (lobbyPlayerUI)
+            //Destroy(lobbyPlayerUI.gameObject);
         if (LobbyList._instance)
             LobbyList._instance.RemovePlayer(this);
     }
